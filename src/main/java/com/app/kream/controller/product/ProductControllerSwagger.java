623dc0786f12098e01a0cf514faf26cf3a6bd08f
MinjaeLee <2alswo7@khu.ac.kr>
2024-05-13 23:36:54 +0900

    [feat] #6 - 세부 상품 정보 API Controller 구현

--- src/main/java/com/app/kream/controller/product/ProductControllerSwagger.java
+++ src/main/java/com/app/kream/controller/product/ProductControllerSwagger.java
@@ -1,7 +1,22 @@
 package com.app.kream.controller.product;
 
+import com.app.kream.common.CommonResponse;
+import com.app.kream.service.dto.DetailProductResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Tag(name = "상품", description = "상품 관련 API 입니다.")
 public interface ProductControllerSwagger {
+    @Operation(summary = "세부 상품 조회 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "세부 상품 조회에 성공했습니다.")
+    })
+    CommonResponse<DetailProductResponse> getDetailProduct(
+            @RequestHeader final Long userId,
+            @PathVariable final Long productId
+    );
 }
