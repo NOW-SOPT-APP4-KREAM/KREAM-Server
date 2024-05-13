548eb80c4592f89e3e0915eed0795b5791bd8777
rlarlgnszx <rlarlgnszx@naver.com>
2024-05-14 03:40:41 +0900

    [feat] #5 - Scrap Controller 구현 및 Annotation 추가

--- src/main/java/com/app/kream/repository/ScrapRepository.java
+++ src/main/java/com/app/kream/repository/ScrapRepository.java
@@ -2,6 +2,8 @@ package com.app.kream.repository;
 
 import com.app.kream.domain.Scrap;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
 }
--- src/main/java/com/app/kream/service/ScrapService.java
+++ src/main/java/com/app/kream/service/ScrapService.java
@@ -1,4 +1,29 @@
 package com.app.kream.service;
 
+import com.app.kream.common.CommonResponse;
+import com.app.kream.domain.Scrap;
+import com.app.kream.exception.SuccessMessage;
+import com.app.kream.repository.ScrapRepository;
+import com.app.kream.service.dto.ScrapCreateDto;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ScrapService {
+    private final ScrapRepository scrapRepository;
+
+    @Transactional
+    public CommonResponse createScrap(
+            ScrapCreateDto request
+    ) {
+        Scrap scrap = Scrap.builder()
+                .memberId(request.memberId())
+                .productId(request.productId())
+                .build();
+
+        scrapRepository.save(scrap);
+        return CommonResponse.success(SuccessMessage.PROCESS_SUCCESS);
+    }
 }
