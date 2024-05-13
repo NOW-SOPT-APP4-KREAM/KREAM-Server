b63ae8427f0b5e17d0e0edbe2d9e07947e73fe9e
MinjaeLee <2alswo7@khu.ac.kr>
2024-05-14 01:16:02 +0900

    [chore] #6 - Swagger 수정

--- src/main/java/com/app/kream/controller/product/ProductControllerSwagger.java
+++ src/main/java/com/app/kream/controller/product/ProductControllerSwagger.java
@@ -3,6 +3,8 @@ package com.app.kream.controller.product;
 import com.app.kream.common.CommonResponse;
 import com.app.kream.service.dto.DetailProductResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,7 +15,12 @@ import org.springframework.web.bind.annotation.RequestHeader;
 public interface ProductControllerSwagger {
     @Operation(summary = "세부 상품 조회 API")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "세부 상품 조회에 성공했습니다.")
+            @ApiResponse(responseCode = "200", description = "세부 상품 조회에 성공했습니다."),
+            @ApiResponse(responseCode = "404",
+                    description = "1. ID에 해당하는 사용자가 존재하지 않습니다.\n" +
+                            "2. ID에 해당하는 상품이 존재하지 않습니다.",
+                    content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류입니다.", content = @Content(schema = @Schema(implementation = CommonResponse.class)))
     })
     CommonResponse<DetailProductResponse> getDetailProduct(
             @RequestHeader final Long memberId,
