0e6d753fa5dd1f6d8cfec0bbc5cbac71cfc9b1a5
rlarlgnszx <rlarlgnszx@naver.com>
2024-05-14 03:36:59 +0900

    [feat] #5 - Scrap Controller & Swagger 구현

--- src/main/java/com/app/kream/controller/scrap/ScrapController.java
+++ src/main/java/com/app/kream/controller/scrap/ScrapController.java
@@ -1,4 +1,22 @@
 package com.app.kream.controller.scrap;
 
+import com.app.kream.common.CommonResponse;
+import com.app.kream.service.ScrapService;
+import com.app.kream.service.dto.ScrapCreateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/scrap")
 public class ScrapController implements ScrapControllerSwagger {
+    private final ScrapService scrapService;
+    @PostMapping
+    public CommonResponse createScrap(
+           ScrapCreateDto request
+    ) {
+        return scrapService.createScrap(
+                request
+        );
+    }
 }
--- src/main/java/com/app/kream/controller/scrap/ScrapControllerSwagger.java
+++ src/main/java/com/app/kream/controller/scrap/ScrapControllerSwagger.java
@@ -1,7 +1,27 @@
 package com.app.kream.controller.scrap;
 
+import com.app.kream.common.CommonResponse;
+import com.app.kream.domain.Scrap;
+import com.app.kream.service.dto.ScrapCreateDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "스크랩", description = "스크랩 관련 API 입니다.")
 public interface ScrapControllerSwagger {
+    @Operation(summary = "세부 상품 조회 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Scarp 생성에 성공했습니다."),
+            @ApiResponse(responseCode = "404",
+                    description = "1. Scrap 생성 실패.\n",
+                    content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류입니다.", content = @Content(schema = @Schema(implementation = CommonResponse.class)))
+    })
+    CommonResponse createScrap(
+            ScrapCreateDto request
+    );
+
 }
