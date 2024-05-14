1e17bc0f1f8daff39dc5e1bc6c29d1e2ae9b6040
rlarlgnszx <rlarlgnszx@naver.com>
2024-05-14 18:06:49 +0900

    [chore] #5 - 구현 후작업으로 변경

--- src/main/java/com/app/kream/controller/scrap/ScrapControllerSwagger.java
+++ src/main/java/com/app/kream/controller/scrap/ScrapControllerSwagger.java
@@ -1,8 +1,7 @@
 package com.app.kream.controller.scrap;
 
 import com.app.kream.common.CommonResponse;
-import com.app.kream.domain.Scrap;
-import com.app.kream.service.dto.ScrapCreateDto;
+import com.app.kream.service.dto.ScrapCreateRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -12,16 +11,17 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "스크랩", description = "스크랩 관련 API 입니다.")
 public interface ScrapControllerSwagger {
-    @Operation(summary = "세부 상품 조회 API")
+    @Operation(summary = "스크랩 상품 등록 API")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Scarp 생성에 성공했습니다."),
-            @ApiResponse(responseCode = "404",
-                    description = "1. Scrap 생성 실패.\n",
-                    content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+            //TODO
+//            @ApiResponse(responseCode = "404",
+//                    description = "1. Scrap 생성 실패.\n"
+//            ),
             @ApiResponse(responseCode = "500", description = "서버 내부 오류입니다.", content = @Content(schema = @Schema(implementation = CommonResponse.class)))
     })
     CommonResponse createScrap(
-            ScrapCreateDto request
+            ScrapCreateRequest request
     );
 
 }
