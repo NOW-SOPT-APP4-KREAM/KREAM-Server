b134a0a798a71a2134a86d9e27c4cdcd8fac9f0f
rlarlgnszx <rlarlgnszx@naver.com>
2024-05-14 18:06:01 +0900

    [chore] #5 - 줄 바꿈 처리

--- src/main/java/com/app/kream/controller/scrap/ScrapController.java
+++ src/main/java/com/app/kream/controller/scrap/ScrapController.java
@@ -2,7 +2,7 @@ package com.app.kream.controller.scrap;
 
 import com.app.kream.common.CommonResponse;
 import com.app.kream.service.ScrapService;
-import com.app.kream.service.dto.ScrapCreateDto;
+import com.app.kream.service.dto.ScrapCreateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,10 +10,12 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/scrap")
 public class ScrapController implements ScrapControllerSwagger {
+
     private final ScrapService scrapService;
+
     @PostMapping
     public CommonResponse createScrap(
-           ScrapCreateDto request
+            ScrapCreateRequest request
     ) {
         return scrapService.createScrap(
                 request
