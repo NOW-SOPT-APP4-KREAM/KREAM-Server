eaf7f6b33033d59f6e4f16e7c3da61a4c2028f55
rlarlgnszx <rlarlgnszx@naver.com>
2024-05-14 18:05:31 +0900

    [refactor] #5- DTO 이름 변경

--- src/main/java/com/app/kream/service/dto/ScrapCreateRequest.java
+++ src/main/java/com/app/kream/service/dto/ScrapCreateRequest.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @Getter
-public record ScrapCreateDto (
+public record ScrapCreateRequest(
         @RequestHeader Long memberId,
         @RequestBody Long productId
 ) {
