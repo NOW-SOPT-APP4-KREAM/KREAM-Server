ac927f8f5c53979fdbd423e91b5ef65f2442df2f
MinjaeLee <2alswo7@khu.ac.kr>
2024-05-14 21:37:32 +0900

    [feat] #9 - 발매정보 조회 Controller부 구현

--- src/main/java/com/app/kream/controller/product/ProductController.java
+++ src/main/java/com/app/kream/controller/product/ProductController.java
@@ -4,6 +4,7 @@ import com.app.kream.common.CommonResponse;
 import com.app.kream.exception.SuccessMessage;
 import com.app.kream.service.ProductService;
 import com.app.kream.service.dto.DetailProductResponse;
+import com.app.kream.service.dto.ReleaseProductResponse;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,5 +25,13 @@ public class ProductController implements ProductControllerSwagger {
             @PathVariable final Long productId
     ) {
         return CommonResponse.success(SuccessMessage.GET_DETAIL_PRODUCT_SUCCESS, productService.findDetailProductById(memberId, productId));
+    }
+
+    @Override
+    @GetMapping("/release")
+    public CommonResponse<ReleaseProductResponse> getReleaseProduct(
+            @RequestHeader final Long memberId
+    ) {
+        return CommonResponse.success(SuccessMessage.GET_RELEASE_PRODUCT_SUCCESS, productService.findReleaseProductById(memberId));
     }
 }
