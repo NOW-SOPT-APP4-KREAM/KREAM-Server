d9cf118015072a3d915841d0c867ae9bd7dca047
rlarlgnszx <rlarlgnszx@naver.com>
2024-05-15 16:49:28 +0900

    [feat] #10- delete기능에 따른 Success , ErrorMessage 추가

--- src/main/java/com/app/kream/exception/SuccessMessage.java
+++ src/main/java/com/app/kream/exception/SuccessMessage.java
@@ -14,13 +14,7 @@ public enum SuccessMessage {
     PROCESS_SUCCESS(HttpStatus.OK.value(), "OK"),
     GET_DETAIL_PRODUCT_SUCCESS(HttpStatus.OK.value(), "세부 상품 조회에 성공했습니다."),
     GET_RELEASE_PRODUCT_SUCCESS(HttpStatus.OK.value(), "발매 상품 정보 조회에 성공했습니다."),
-
-    /**
-     * 201 CREATED SUCCESS
-     */
-
-
-    ;
+    DELETE_SCRAP_SUCCESS(HttpStatus.OK.value(), "스크랩 제거에 성공했습니다.");
 
     private final int status;
     private final String message;
