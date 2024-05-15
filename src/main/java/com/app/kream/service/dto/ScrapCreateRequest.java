480f654b0c21463690fcb76bd6c8aa991650c3cf
rlarlgnszx <rlarlgnszx@naver.com>
2024-05-15 16:51:12 +0900

    [feat] #11 - ScrapCreateRequest  수정 및 ScrapDeleteRequest 구현

--- src/main/java/com/app/kream/service/dto/ScrapCreateRequest.java
+++ src/main/java/com/app/kream/service/dto/ScrapCreateRequest.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 public record ScrapCreateRequest(
-        @RequestHeader Long memberId,
-        @RequestBody Long productId
+        Long productId
 ) {
 }
